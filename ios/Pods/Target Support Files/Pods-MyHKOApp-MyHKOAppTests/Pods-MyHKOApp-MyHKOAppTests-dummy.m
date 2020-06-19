425e58dff3a70b9efe1ab4c88b27e958ce60dfa7
Chun Kin Lee <weconnecthk@gmail.com>
2020-06-20 00:33:52 +0800

    Init React Native Project

---
+++ ios/Pods/Target Support Files/Pods-MyHKOApp-MyHKOAppTests/Pods-MyHKOApp-MyHKOAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyHKOApp_MyHKOAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_MyHKOApp_MyHKOAppTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-MyHKOApp-tvOSTests/Pods-MyHKOApp-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyHKOApp_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_MyHKOApp_tvOSTests
+@end
